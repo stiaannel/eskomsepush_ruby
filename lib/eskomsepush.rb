64e80373d08d0369f3cd109895b036257dfc1f13
stiaannel <christiaannel9@gmail.com>
2023-06-15 20:03:17 +0200

    update gemspec

--- lib/eskomsepush.rb
+++ lib/eskomsepush.rb
@@ -5,4 +5,9 @@ require_relative "eskomsepush/version"
 module Eskomsepush
   class Error < StandardError; end
   # Your code goes here...
+  class Setup
+    def self.greet
+      puts "Hello World"
+    end
+  end
 end
